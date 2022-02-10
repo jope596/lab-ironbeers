e7b048e8009576bd9e1e3a2df99f906a8b271c47
José Pedro Pereira <ze.pereira96@gmail.com>
2022-02-10 10:08:18 +0000

    done

--- app.js
+++ app.js
@@ -7,12 +7,38 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+
+
+
+app.get('/beers', (req, res, next) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi =>{
+ console.log('Beers from the database: ', beersFromApi);
+  res.render('beers', {beersFromApi} );
+})
+.catch(error => console.log(error));
+});
+
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(beersFromApi =>{
+    console.log('Beers from the database: ', beersFromApi);
+  res.render('beers', {beersFromApi} );
+})
+.catch(error => console.log(error));
+});
+ 
 
 // ...
 
@@ -22,4 +48,6 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'))
